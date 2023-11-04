d3d17f7e21f093716c1c1d5d0810cc20e1f3479d
lucasMuner <146034160+lucasMuner@users.noreply.github.com>
2023-11-04 13:20:04 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -7,11 +7,7 @@ const socketIo = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server, {
-  cors: {
-    origin: "*",
-  }
-});
+const io = socketIo(server);
 const port = 3000; 
 app.use(cors());
 app.use(express.json());
